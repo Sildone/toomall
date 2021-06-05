07e58efbd2dd9d8c4a17f3e6fdcbd3348bb2187e
Sildone <sildone@aliyun.com>
2021-06-06 00:01:07 +0800

    update

--- toomall-gateway/src/main/java/cn/t00strive/toomall/gateway/ToomallGatewayApplication.java
+++ toomall-gateway/src/main/java/cn/t00strive/toomall/gateway/ToomallGatewayApplication.java
@@ -3,20 +3,23 @@ package cn.t00strive.toomall.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
+ * ToomallGatewayApplication
  *
- */
+ * @author Sildone
+ * @date 2021/06/05 23:08
+ **/
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ToomallGatewayApplication {
-	/**
-	 *
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(ToomallGatewayApplication.class, args);
-	}
+    /**
+     * main
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ToomallGatewayApplication.class, args);
+    }
 
 }
---
+++ toomall-gateway/src/main/java/cn/t00strive/toomall/gateway/filter/TokenGlobalFilter.java
@@ -0,0 +1,28 @@
+package cn.t00strive.toomall.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * TokenGlobalFilter
+ *
+ * @author duansg
+ * @version 1.0
+ * @date 2021/6/6 上午12:00
+ */
+@Component
+public class TokenGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
